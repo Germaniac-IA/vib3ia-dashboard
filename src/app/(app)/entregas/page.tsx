b59666568eb8f705d798a05adc9be0e79c81910e
Luz-VIB3 <luz.foxxie@gmail.com>
2026-04-14 13:30:32 -0300

    Fix entregas import path

--- src/app/(app)/entregas/page.tsx
+++ src/app/(app)/entregas/page.tsx
@@ -1,14 +1,1 @@
-"use client";
-
-import { PageTitle, Card, Empty } from "../../../components/UI";
-
-export default function EntregasPage() {
-  return (
-    <div>
-      <PageTitle>🚚 Entregas</PageTitle>
-      <Card>
-        <Empty message="Módulo de entregas en desarrollo" />
-      </Card>
-    </div>
-  );
-}
+"use client";import { PageTitle, Card, Empty } from "../../components/shared/UI";export default function EntregasPage() {  return (    <div>      <PageTitle>🚚 Entregas</PageTitle>      <Card>        <Empty message="Módulo de entregas en desarrollo" />      </Card>    </div>  );}
